debfa66e849adc8b738788e9fb0ad70fac0759c0
agn2017331008 <ghosharpita423@gmail.com>
2023-05-31 10:34:01 +0600

    test commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  devise_for :providers, controllers: {
+    sessions: 'providers/sessions'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "home#index"
+end
